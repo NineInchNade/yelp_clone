c07f648fab94766944818503827d6fb31f67e8e0
NineInchNade <nineinchnade@gmail.com>
2014-10-02 19:51:32 +0100

    added star rating helper that displays the average rating as stars (all test passing)

--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -1,7 +1,9 @@
 module ReviewsHelper
 
 	def star_rating(rating)
-		rating
-	end
+		return rating unless rating.respond_to?(:round)
+		remainder = (5 - rating.round)
+		"★" * rating.round + ('☆' * remainder)
+ 	end
 end
 
